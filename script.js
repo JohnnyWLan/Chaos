cc5bee726421c79e10e95b3db9c925292e0732f1
JohnnyWLan <130187009+JohnnyWLan@users.noreply.github.com>
2024-01-06 14:51:16 -0600

    adding starting project

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+// let enternow = document.querySelector("enternow");
+
+// function logit(){
+// 	console.log('hello')
+// }
+
+// enternow.addEventListener("click", logit);
+
+function myFunction() {
+	console.log("hello")
+};
+
+
+document.getElementById("myButton").addEventListener("click", myFunction);
+
