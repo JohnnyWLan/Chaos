2589748f70e9d19d6a7cea7400a1b3efbaaf1aad
JohnnyWLan <130187009+JohnnyWLan@users.noreply.github.com>
2024-01-07 10:24:44 -0600

    added bundle and json

--- script.js
+++ script.js
@@ -10,7 +10,9 @@
 
 // enternow.addEventListener("click", logit);
 var _ = require('lodash');
-console.log(_);
+
+let array = [1,2,3,4,5,6,7,8];
+console.log('answer:',_.without(array,3));
 function myFunction() {
 	console.log("hello")
 };
